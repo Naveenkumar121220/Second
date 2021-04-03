a21b2742919427f5f80a8df8dfd8da72ae9692bc
Naveen <ramisetty.brothers@gmail.com>
2021-04-03 17:31:04 +0530

    add existing file

---
+++ RestaurantManagmentDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagment.Models
+{
+    public partial class RestaurantManagmentDbContext:DbContext
+    {
+        public RestaurantManagmentDbContext()
+
+            : base("RMS_DB")
+
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+
+        {
+
+        }
+
+
+    }
+}
